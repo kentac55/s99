bc1edadf8d2dae47ba369297b1befe91ad4323ed
kentac55 <dev.kentac55@gmail.com>
2017-05-28 08:31:38 +0900

    use pattern match

--- src/main/scala/P04.scala
+++ src/main/scala/P04.scala
@@ -1,13 +1,9 @@
 object P04 {
   def length[T](l: List[T]): Int = {
-    def recLength[A](l: List[A], acc: Int = 0): Int = {
-      if (l == Nil) {
-        acc
-      } else {
-        recLength(l.tail, acc + 1)
-      }
+    def recLength[A](lst: List[A], acc: Int = 0): Int = lst match {
+      case Nil => acc
+      case _   => recLength(lst.tail, acc + 1)
     }
-
     recLength(l)
   }
 }
